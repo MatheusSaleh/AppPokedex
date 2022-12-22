b8865905e4b8fce09893518448c9e1c37b2b45e7
MatheusSaleh <matheusxmg2@gmail.com>
2022-12-22 10:14:28 -0300

    Primeiro Commit

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,27 @@
+import { PokeApiService } from './../../service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+
+export class PokeListComponent implements OnInit {
+
+  public getAllPokemons: any;
+  constructor(
+    private PokeApiService: PokeApiService
+  ){
+
+
+  }
+  ngOnInit(): void{
+    this.PokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results;
+      }
+    );
+  }
+
+}
